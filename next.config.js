72f7a72e7fbaa37bc0703a0ac086a3a3c43ef2bb
ConnectSilveyraDispatch <connect@silveyradispatch.com>
2026-04-26 19:43:48 -0600

    Remove experimental turbo from next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      root: 'C:/Users/karla/Desktop/silveyra-dispatch'
-    }
-  }
+  reactStrictMode: true
 }
-
-module.exports = nextConfig
